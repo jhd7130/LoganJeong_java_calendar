8eb36fd383998b364910b240823baf442dcb129f
jeongheedoo <jeongheedoo@218.38.137.27>
2021-03-12 14:51:41 +0900

    Add to use Scanner

--- src/honux/calendar/Sum.java
+++ src/honux/calendar/Sum.java
@@ -10,15 +10,17 @@ public class Sum {
 		int a,b;
 		System.out.println("두수를 입력하시오");
 		Scanner scan= new Scanner(System.in);
-		
 		String s1,s2;
 		s1 = scan.next();
 		s2 = scan.next();
-		
 		a=Integer.parseInt(s1);
 		b = Integer.parseInt(s2);
+		
 		int add = a+b;
+		int dividend = a/b;
+		
 		System.out.println("두수의 합은 : " + add + "입니다.");
 		scan.close();
 	}
 }
+ 
