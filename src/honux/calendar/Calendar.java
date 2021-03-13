c8f9ebad2393c7981084ac2a57fdb1d8f5c80a1e
jeongheedoo <jeongheedoo@218.38.137.27>
2021-03-13 09:53:29 +0900

    repeat Calendar with mine(used Arrays)

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,31 +3,40 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-
-	private static final int[] maxDayOfMonth = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
-
-	public int getmaxDay(int month) {
-		return maxDayOfMonth[month - 1];
+	
+	private final int[] maxDays = {31, 28,31,30,31,30,31,30,31,30,31,30};
+	
+	public int maxDaysOfM(int month) {
+		return maxDays[month-1];
 	}
-
-	private static void calendar() {
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("--------------------");
-		System.out.println("   1   2    3  4   5   6   7");
-		System.out.println("  8   9  10  11 12  13  14");
-		System.out.println(" 15  16  17 18 19 20  21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-
+	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		System.out.println("원하는 달을 입력하시오.");
+	Scanner scanner = new Scanner(System.in);
+	Calendar cal = new Calendar();
+	
+	System.out.println("반복횟수를 입력하시오.");
+	int repeat = scanner.nextInt();
+	
+	System.out.println("원하는 달을 입력하세요.");
+	for( int i= 0; i < repeat;i++) {
 		int month = scanner.nextInt();
-
-		System.out.printf("%d월달은 %d 일 입니다%n%n", month, cal.maxDayOfMonth[month]);
-		cal.calendar();
-		scanner.close();
-
+		System.out.printf("%d달은  %d일 입니다.%n",month,cal.maxDaysOfM(month));
+	}
+	System.out.println("시스템이 끝났습니다.");
+	scanner.close();
 	}
 }
+/*  내가 한거
+Scanner scan= new Scanner(System.in);
+
+System.out.println("반복 횟수를 입력하세요.");
+int count = scan.nextInt();
+
+System.out.println("월을 입력하세요.");
+int[] countT = new int[count];
+	for(int i=0; i<countT.length; i++) {
+		countT[i]=scan.nextInt();
+		}
+int[] max_month = {31,28,30,31,30,31,30,31,30,31,30,31};
+for(int j=0; j<countT.length;j++)
+	System.out.printf("%d월은 %d일 입니다.%n",countT[j],max_month[countT[j]-1]); */
